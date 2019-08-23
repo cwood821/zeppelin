583e89213a814adb5ebfe9a7a742bc991fc5ea6a
Christian Wood <christian@christianwood.net>
2019-08-23 08:52:17 -0500

    Make requests async

--- src/main.rs
+++ src/main.rs
@@ -1,39 +1,40 @@
 use std::env;
 
+use reqwest::r#async::Client; // 0.9.14
 mod status_check;
 use status_check::StatusCheck;
-
 mod url_repository;
 use url_repository::UrlRepository;
-
 mod notifier;
 use notifier::Notifier;
 
-fn main() -> Result<(), Box<dyn std::error::Error>> {
+type Result<T> = std::result::Result<T, Box<dyn std::error::Error>>;
+
+fn main() -> Result<()> {
     let args: Vec<String> = env::args().collect();
 
-    // TODO: Error handling for args
+    if args.len() < 2 {
+        panic!("Two arguments required: url file and slack endpoint");
+    }
+
     let url_file = &args[1];
     let slack_url = &args[2];
+
+    let urls = match UrlRepository::from_file(url_file) {
+        Ok(urls) => urls,
+        Err(_) => panic!("Failed to retrieve urls from the url file") 
+    };
 
     let notifier = Notifier {
         url: slack_url.to_string()
     };
 
-    let urls = match UrlRepository::from_file(url_file) {
-        Ok(urls) => urls,
-        Err(_) => panic!("No urls in the urls file") 
+    let status_check = StatusCheck {
+        client: Client::new(),
+        notifier: notifier
     };
 
-    for url in &urls {
-        match StatusCheck::is_ok(&url) {
-           true => {},
-           false => {
-                let message = format!("🚨 {} is down!", url);
-                notifier.notify(&message);
-           } 
-        }
-    }
-
+    status_check.check(urls);
+    
     Ok(())
 }
--- src/status_check.rs
+++ src/status_check.rs
@@ -1,20 +1,46 @@
 extern crate reqwest;
+use futures::{stream, Future, Stream};
+use reqwest::r#async::Client;
+use tokio;
+use crate::notifier::Notifier;
 
-pub struct StatusCheck {}
+pub struct StatusCheck {
+  pub client: Client,
+  pub notifier: Notifier
+}
 
 impl StatusCheck {
 
-  pub fn is_ok(url: &str) -> bool {
-    StatusCheck::get_status(&url)
-          .map_err(|_| false)
-          .and_then(|status| Ok(status == reqwest::StatusCode::OK))
-          .unwrap()
-  }
+  pub fn check(self, urls: Vec<String>) {
+    const PARALLEL_REQUESTS: usize = 2;
 
-  // TODO: remove hard dependency on reqwest types
-  pub fn get_status(url: &str) -> Result<reqwest::StatusCode, reqwest::Error> {
-      let resp = reqwest::get(url)?;
-      Ok(resp.status())
+    let responses = stream::iter_ok(urls)
+        .map(move |url| {
+            let client = Client::new();
+            client
+                .get(&url)
+                .send()
+        })
+        .buffer_unordered(PARALLEL_REQUESTS);
+
+    let work = responses
+        .for_each(move |r| {
+            // Note: panics if a connection is failed to establish
+            // Need to handle case where a domain is no longer hosted
+            // https://docs.rs/tokio/0.1.22/tokio/runtime/struct.Builder.html
+            match r.status().is_success() {
+              true => {},
+              false => {
+                    let message = format!("🚨 {} is down!", r.url());
+                    self.notifier.notify(&message);
+              } 
+            }
+            println!("Got {} from {}", r.status(), r.url());
+            Ok(())
+        })
+        .map_err(|e| panic!("Error while processing: {}", e));
+
+      tokio::run(work);
   }
 
 }
